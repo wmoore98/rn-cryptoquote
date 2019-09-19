09691bfa1f44ca20367a6b88829b8ccdde138834
Bill <wmoore98@sbcglobal.net>
2019-09-19 13:53:43 -0400

    Updates

---
+++ precache-manifest.c1f75749c03de031e5e15b0bf8bf43f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7506e5bc5966b3edd2bf25395c292847",
+    "url": "/rn-cryptoquote/index.html"
+  },
+  {
+    "revision": "37f9fda0fce1fe1141fb",
+    "url": "/rn-cryptoquote/static/css/main.1d51b65c.chunk.css"
+  },
+  {
+    "revision": "7ed3ee5f03ac4c697528",
+    "url": "/rn-cryptoquote/static/js/2.90b876f4.chunk.js"
+  },
+  {
+    "revision": "37f9fda0fce1fe1141fb",
+    "url": "/rn-cryptoquote/static/js/main.9d7ac1bc.chunk.js"
+  },
+  {
+    "revision": "b4638cd704afab7ce876",
+    "url": "/rn-cryptoquote/static/js/runtime~main.5e1ed734.js"
+  }
+]);
